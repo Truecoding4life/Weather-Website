037bb85e1750a60177a51a9aff16e95e48c5d1bb
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-10-08 06:52:19 -0700

    Scratch Java script, using callback function

--- asset/script.js
+++ asset/script.js
@@ -1,83 +1,47 @@
 var search = $('input[name="userInput"]');
+var requestUrl;
 $('#searchButton').on('click', function (event) {
     event.preventDefault;
     search = search.val();
     console.log(search);
-    requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + search.replace(/\s/g, '').toLowerCase() + ',+1&limit=5&appid=f60935b102f3c62d074d56612d72ebbd';
+    fetchToGeo();
 
-    getAPI();
-});
- function getAPI() {
-    function API() {
-        var locationLat, locationLon;
-        var returnData;
+    function fetchToGeo() {
+
+        requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + search.replace(/\s/g, '').toLowerCase() + ',+1&limit=5&appid=f60935b102f3c62d074d56612d72ebbd';
+        var LatIs, LonIs, returnData;
         fetch(requestUrl)
-            .then(function (response) {
-                console.log("Request Link is :  " + requestUrl);
-                return response.json();
-
-            }).then(function (data) {
-                console.log("Data received is   " + data,data);
+            .then(function (response){
+             if (response.ok) {
+                response.json().then(function (data) {
                 returnData = data;
-                console.log(returnData,data);
-                console.log("Return Data is   " + returnData,data);
-                locationLat = returnData[0].lat.toFixed(2);
-                locationLon = returnData[0].lon.toFixed(2);
-                console.log("returned lat is: " + locationLat);
-                console.log("returned lon is : " + locationLon);
-                console.log('Lat and Lot is Fetched')
-                weatherData();
-
+                console.log(returnData, data);
+                LatIs = returnData[0].lat.toFixed(2);
+                LonIs = returnData[0].lon.toFixed(2);
+                fetchtoData(LatIs, LonIs);
             })
-        function weatherData (){
-            var newUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+locationLat+'&lon=' + locationLon+'&appid=f60935b102f3c62d074d56612d72ebbd'
-            var allData;
-            console.log("This URL WILL BE USE TO FETCH LOCATION  " + newUrl);
-            fetch(newUrl)
-                .then(function(response){
-                    console.log('RETURN RESPONSE IS'+ response);
-                    return response.json();
-                })
-                .then(function(data){
-                    console.log(data,data);
-                    allData=data;
-                    console.log('data inside allData is:   '+allData,data);
-                    console.log(allData.list[1]);
-                    for(var i=0; i<4; i++){
-                        console.log(allData.list[i]);
-                        var titLe = $('<div>');
-                        titLe.text(allData.city.name);
-                        $('#day1').append(titLe);
-                        $('#day1').text(allData[i].dt_txt);
-                        // $('#weatherdisplay').append($('<div>')).addClass("card mt-3 p-3 m-4");
-                        // // $('#weatherdisplay > div').append($('<h3>'))
-                        // allData.city[1]
-                    }
-                })
+
+        } else {
+            alert('Error' + response.statusText);
+            console.log('Initital Fetch status is:  ' + response.status);}
+
+
+function fetchtoData(a, b) {
+    requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + a + '&lon=' + b + '&appid=f60935b102f3c62d074d56612d72ebbd';
+    fetch(requestUrl) 
+        .then(function (response) {
+        if (response.ok) {
+            response.json()
+            .then(function (data) {
+                allData = data;
+                console.log(allData,data);})
+        } else {
+            alert('Error ' + response.statusText);
         }
-    }    
-    API();
-    // setTimeout(function () {
-    //     console.log("FUNCTION TIMER ACTIVATED");
-    //     var newUrl = 'api.openweathermap.org/data/2.5/forecast?lat='+locationLat+'&lon=' + locationLon+'&appid=f60935b102f3c62d074d56612d72ebbd'
-    //     var justtest;
-    //     console.log(newUrl);
-    //     fetch(newUrl)
-    //         .then(function(response){
-    //             console.log('RETURN RESPONSE IS'+ response);
-    //             return response.json();
-    //         })
-    //         .then(function(data){
-    //             justtest=data;
-    //             console.log(justtest);
-    //         })
-    // }, 300);
-}
+    })
+
+    }
+    })
 
 
-    // function getforecast () {
-    //     requestUrl= 'api.openweathermap.org/data/2.5/forecast?lat='+locationLat+'&lon=' + locationLon+'&appid=f60935b102f3c62d074d56612d72ebbd';
-    //     console.log("Request for 5 day forecast link is:    " + requestUrl);
-    //     return requestUrl;
-    // }
-    // return (locationLat, locationLon);
+    }})
