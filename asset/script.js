f617f79a89454ccfde9520a761ed9883762a3b38
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-10-09 18:49:23 -0700

    new button align center

--- asset/script.js
+++ asset/script.js
@@ -56,7 +56,7 @@ cities.forEach(city => {
                                                 cities.push(cityName);
                                             localStorage.setItem("cities", JSON.stringify(cities));
                                             var button = $('<button>');
-                                            button.text(cityName);
+                                            button.text(cityName).addClass('col-6 align-items-center');
                                             button.val(cityName);
 
                                             buttoncontainer.append(button);
